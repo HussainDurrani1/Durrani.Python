8f64d665bd0dbcb73f39cdb87080034eb443cd2b
hussaindurrani <hussaindurrani85@gmail.com>
2024-09-23 18:27:33 +0500

    .

---
+++ Durrani_Python_Assignments/Calculator.py
@@ -0,0 +1,11 @@
+Num1 : int = 6
+Num2 : int = 9
+sum : int = Num1+Num2
+dif : int = Num1-Num2
+mul : int = Num1*Num2
+div : float = Num1/Num2 
+print(f"The sum of {Num1} and {Num2} is {sum}")
+print(f"The difference of {Num1} and {Num2} is {dif}")
+print(f"The product of {Num1} and {Num2} is {mul}")
+print(f"The division of {Num1} and {Num2} is {div}")
+print(f"You are: \nWonderful")
